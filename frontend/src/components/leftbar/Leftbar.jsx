1556a2bca1c5fb0415bf00f2950ce9ebda0cfd63
Dasuni Siriwardhana <123971483+dasu97@users.noreply.github.com>
2023-03-12 23:31:22 +0530

    Add files via upload

---
+++ frontend/src/components/leftbar/Leftbar.jsx
@@ -0,0 +1,97 @@
+import "./leftbar.scss"
+import Friends from "../../assets/1.png";
+import Groups from "../../assets/2.png";
+import Market from "../../assets/3.png";
+import Watch from "../../assets/4.png";
+import Memories from "../../assets/5.png";
+import Events from "../../assets/6.png";
+import Gaming from "../../assets/7.png";
+import Galery from "../../assets/8.png";
+import Videos from "../../assets/9.png";
+import Messages from "../../assets/10.png";
+import Fundraiser from "../../assets/13.png";
+import Tutorials from "../../assets/11.png";
+import Courses from "../../assets/12.png";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContex";
+
+
+const Leftbar = () => {
+
+  const { currentUser } = useContext(AuthContext);
+
+  return (
+    <div className="leftbar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+          <img src={currentUser.profilePic} alt=""/>
+          <span>{currentUser.name}</span>
+          </div>
+          <div className="item">
+            <img src={Friends} alt=""/>
+            <span>friends</span>
+          </div>
+          <div className="item">
+            <img src={Groups} alt=""/>
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <img src={Market} alt=""/>
+            <span>Marketplace</span>
+          </div>
+          <div className="item">
+            <img src={Watch} alt=""/>
+            <span>Watch</span>
+          </div>
+          <div className="item">
+            <img src={Memories} alt=""/>
+            <span>Memories</span>
+          </div>
+        </div>
+        <hr/>
+        <div className="menu">
+          <span>Your shortcuts</span>
+          <div className="item">
+            <img src={Events} alt=""/>
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <img src={Gaming} alt=""/>
+            <span>Gaming</span>
+          </div>
+          <div className="item">
+            <img src={Galery} alt=""/>
+            <span>Galery</span>
+          </div>
+          <div className="item">
+            <img src={Videos} alt=""/>
+            <span>Videos</span>
+          </div>
+          <div className="item">
+            <img src={Messages} alt=""/>
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr/>
+        <div className="menu">
+          <span>Others</span>
+          <div className="item">
+            <img src={Fundraiser} alt=""/>
+            <span>Fundraiser</span>
+          </div>
+          <div className="item">
+            <img src={Tutorials} alt=""/>
+            <span>Tutorials</span>
+          </div>
+          <div className="item">
+            <img src={Courses} alt=""/>
+            <span>Courses</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Leftbar
