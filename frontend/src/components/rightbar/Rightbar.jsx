cb59ea999f41ccebc844eaa3d7cfd6be11ae55aa
Dasuni Siriwardhana <123971483+dasu97@users.noreply.github.com>
2023-03-20 23:38:38 +0530

    Add files via upload

---
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,113 @@
+import "./rightbar.scss"
+
+const Rightbar = () => {
+  return (
+    <div className="rightbar">
+     <div className="container">
+      <div className="item">
+        <span>Suggestions for you</span>
+        <div className="user">
+          <div className="userInfo">
+          <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+          <span>Dasuni Siriwardhana</span>
+          </div>
+          <div className="buttons">
+            <button>follow</button>
+            <button>dismiss</button>
+          </div>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+          <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+          <span>Dasuni Siriwardhana</span>
+          </div>
+          <div className="buttons">
+            <button>follow</button>
+            <button>dismiss</button>
+          </div>
+        </div>
+      </div>
+      <div className="item">
+        <span>Latest activities</span>
+        <div className="user">
+          <div className="userInfo">
+          <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+          <p>
+          <span>Dasuni Siriwardhana</span> changed their cover picture
+          </p>
+          </div>
+           <span>1 min ago</span>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+          <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+          <p>
+          <span>Dasuni Siriwardhana</span> changed their cover picture
+          </p>
+          </div>
+           <span>1 min ago</span>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+          <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+          <p>
+          <span>Dasuni Siriwardhana</span> changed their cover picture
+          </p>
+          </div>
+           <span>1 min ago</span>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+          <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+          <p>
+          <span>Dasuni Siriwardhana</span> changed their cover picture
+          </p>
+          </div>
+           <span>1 min ago</span>
+        </div>
+        
+      </div>
+      <div className="item">
+        <span>Online Friends</span>
+        <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+            <div className="online"/>
+              <span>Dasuni Siriwardhana</span>
+          </div>  
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+            <div className="online"/>
+              <span>Dasuni Siriwardhana</span>
+          </div>  
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+            <div className="online"/>
+              <span>Dasuni Siriwardhana</span>
+          </div>  
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+            <div className="online"/>
+              <span>Dasuni Siriwardhana</span>
+          </div>  
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="https://images.pexels.com/photos/9374416/pexels-photo-9374416.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""/>
+            <div className="online"/>
+              <span>Dasuni Siriwardhana</span>
+          </div>  
+        </div>
+      </div>
+     </div>
+    </div>
+  )
+}
+
+export default Rightbar
